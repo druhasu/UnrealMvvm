97661a03fe1b6dfa631ec364bf2ca683a12eb14e
Andrei Sudarikov <druhasu@users.noreply.github.com>
2021-05-13 01:55:07 +0300

    Simplified module definition

--- UnrealMvvm/Source/UnrealMvvm/Private/UnrealMvvm.cpp
+++ UnrealMvvm/Source/UnrealMvvm/Private/UnrealMvvm.cpp
@@ -1,20 +1,5 @@
 // Copyright Andrei Sudarikov. All Rights Reserved.
 
-#include "UnrealMvvm.h"
+#include "Modules/ModuleManager.h"
 
-#define LOCTEXT_NAMESPACE "FUnrealMvvmModule"
-
-void FUnrealMvvmModule::StartupModule()
-{
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
-}
-
-void FUnrealMvvmModule::ShutdownModule()
-{
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
-}
-
-#undef LOCTEXT_NAMESPACE
-	
-IMPLEMENT_MODULE(FUnrealMvvmModule, UnrealMvvm)
+IMPLEMENT_MODULE(FDefaultModuleImpl, UnrealMvvm)
