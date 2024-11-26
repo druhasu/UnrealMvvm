9b8b0e460fd9aa339e9bf009fef8402e1ced9a0b
Andrei Sudarikov <druhasu@users.noreply.github.com>
2024-11-27 01:03:08 +0300

    Code cleanup

--- UnrealMvvm/Source/UnrealMvvm/Public/Mvvm/Impl/Property/PropertyTypeSelector.h
+++ UnrealMvvm/Source/UnrealMvvm/Public/Mvvm/Impl/Property/PropertyTypeSelector.h
@@ -2,26 +2,16 @@
 
 #pragma once
 
-#include "Templates/ChooseClass.h"
 #include "Templates/IsArithmetic.h"
 #include "Mvvm/ViewModelPropertyTypeTraits.h"
-#include "Misc/EngineVersionComparison.h"
-
-#if !UE_VERSION_OLDER_THAN(5,4,0)
 #include <type_traits>
-#endif
 
 namespace UnrealMvvm_Impl
 {
 
     // helper used to select by-value setter for arithmetic types
-//#if UE_VERSION_OLDER_THAN(5,4,0)
-    //template <typename T>
-    //using TByValueOrByRef = TChooseClass< TIsArithmetic<T>::Value || TIsEnum<T>::Value || TViewModelPropertyTypeTraits<T>::WithSetterArgumentByValue, T, const T& >::Result;
-//#else
     template <typename T>
     using TByValueOrByRef = std::conditional_t< TIsArithmetic<T>::Value || TIsEnum<T>::Value || TViewModelPropertyTypeTraits<T>::WithSetterArgumentByValue, T, const T& >;
-//#endif
 
     template <typename T>
     struct TPropertyTypeSelector
